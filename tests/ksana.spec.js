e57e7efdcbb39934d4e76ed6966ee3d974b3375e
Mujumdar <mujumdarn@tab101313.corpad.net.local>
2017-04-10 16:02:52 +1000

    added references

--- tests/ksana.spec.js
+++ tests/ksana.spec.js
@@ -18,146 +18,164 @@ describe('Using kSaNa',()=>{
     clock.restore()
     sandbox.restore()
   })
-  describe('Within a time-interval',()=>{
-    describe('An event starts the time window',()=>{
-      const emitter = new eventEmitter();
-      let onEvent = 'on-event'
-      let afterTime = 5
-      let triggerEvent = 'another-event'
-      let cancelEvent = 'cancelation-event'
 
-      describe('If the cancel event is not triggered within the time-interval',()=>{
-        let k
-        before(()=>{
-          k = ksana.on(emitter,onEvent)
-          .afterTime(afterTime)
-          .trigger(triggerEvent)
-        })
-        after(()=>{
-          k.stop()
-        })
-        it('Should trigger the event',(done)=>{
-          let payload = "hello"
-          emitter.on(triggerEvent,(p)=>{
-            expect(p).to.deep.equal(payload)
-            return done()
-          })
-          emitter.emit(onEvent,payload)
-          clock.tick(afterTime)
-        })
+  describe('An event starts the time window',()=>{
+    const emitter = new eventEmitter();
+    let onEvent = 'on-event'
+    let afterTime = 5
+    let triggerEvent = 'result-event'
+    let cancelEvent = 'cancelation-event'
+
+    describe('If the cancel event is not triggered within the time-interval',()=>{
+      let k
+      before(()=>{
+        k = ksana.on(emitter,onEvent)
+        .afterTime(afterTime)
+        .trigger(triggerEvent)
       })
-
-      describe('If the cancel event is triggered within the time-interval',()=>{
-        let k
-        before(()=>{
-          k = ksana.on(emitter,onEvent)
-          .afterTime(afterTime)
-          .trigger(triggerEvent)
-          .cancelOn(cancelEvent)
-        })
-        after(()=>{
-          k.stop()
-        })
-        it('Should NOT trigger the event',(done)=>{
-          let payload = "hello"
-          let triggerEventCalled = false
-          let delta = after/10
-          emitter.on(triggerEvent,(p)=>{
-            triggerEventCalled = true
-          })
-          k._internal.onCancalled = ()=>{
-            expect(triggerEventCalled).to.be.false
-            return done()
-          }
-          emitter.emit(onEvent,payload)
-          clock.tick(afterTime-delta)
-          emitter.emit(cancelEvent,{})
-        })
+      after(()=>{
+        k.stop()
       })
-
-    })
-
-    describe('Counting instances of the event',()=>{
-      const emitter = new eventEmitter();
-      let onEvent = 'on-event'
-      let triggerEvent = 'another-event'
-
-      describe('After "x" instances of the event',()=>{
-        let k
-        let n = 10
-        before(()=>{
-          k = ksana.on(emitter,onEvent)
-          .afterNTimes(n)
-          .trigger(triggerEvent)
-        })
-        after(()=>{
-          k.stop()
-        })
-        it('Should trigger another event',(done)=>{
-          let payload = "hello"
-          emitter.on(triggerEvent,(p)=>{
-            expect(p).to.deep.equal(payload)
-            return done()
-          })
-          for(i=0;i<n;i++) {
-            emitter.emit(onEvent,payload)
-          }
-        })
-      })
-
-      describe('Every "x" instances of the event',()=>{
-        let k
-        let n = 10
-        let cycles = 2
-        before(()=>{
-          k = ksana.on(emitter,onEvent)
-          .afterNTimes(n)
-          .trigger(triggerEvent)
-        })
-        after(()=>{
-          k.stop()
-        })
-        it('Should trigger another event',(done)=>{
-          let payload = "hello"
-          let called = 0
-          emitter.on(triggerEvent,(p)=>{
-            expect(p).to.deep.equal(payload)
-            called = called + 1
-            if(called == cycles) {
-              return done()
-            }
-          })
-          for(i=0;i<n*cycles;i++) {
-            emitter.emit(onEvent,payload)
-          }
-        })
-      })
-
-      describe('After "x" instances and a few more of the event',()=>{
-        let k
-        let n = 10
-        let few = n/2
-        before(()=>{
-          k = ksana.on(emitter,onEvent)
-          .afterNTimes(n)
-          .trigger(triggerEvent)
-        })
-        after(()=>{
-          k.stop()
-        })
-        it('Should still trigger another event only once',(done)=>{
-          let payload = "hello"
-          let called = 0
-          emitter.on(triggerEvent,(p)=>{
-            expect(p).to.deep.equal(payload)
-            called = called + 1
-          })
-          for(i=0;i<n+few;i++) {
-            emitter.emit(onEvent,payload)
-          }
-          expect(called).to.be.equal(1)
+      it('Should trigger the event',(done)=>{
+        let payload = "hello"
+        emitter.on(triggerEvent,(p)=>{
+          expect(p).to.deep.equal(payload)
           return done()
         })
+        emitter.emit(onEvent,payload)
+        clock.tick(afterTime)
+      })
+    })
+
+    describe('If the cancel event is triggered within the time-interval',()=>{
+      let k
+      before(()=>{
+        k = ksana.on(emitter,onEvent)
+        .afterTime(afterTime)
+        .trigger(triggerEvent)
+        .cancelOn(cancelEvent)
+      })
+      after(()=>{
+        k.stop()
+      })
+      it('Should NOT trigger the event',(done)=>{
+        let payload = "hello"
+        let triggerEventCalled = false
+        let delta = after/10
+        emitter.on(triggerEvent,(p)=>{
+          triggerEventCalled = true
+        })
+        k._internal.onCancalled = ()=>{
+          expect(triggerEventCalled).to.be.false
+          return done()
+        }
+        emitter.emit(onEvent,payload)
+        clock.tick(afterTime-delta)
+        emitter.emit(cancelEvent,{})
+      })
+    })
+
+  })
+
+  describe('Counting instances of the event',()=>{
+    const emitter = new eventEmitter();
+    let onEvent = 'on-event'
+    let triggerEvent = 'result-event'
+
+    describe('After "x" instances of the event',()=>{
+      let k
+      let n = 10
+      before(()=>{
+        k = ksana.on(emitter,onEvent)
+        .afterNTimes(n)
+        .trigger(triggerEvent)
+      })
+      after(()=>{
+        k.stop()
+      })
+      it('Should trigger result event',(done)=>{
+        let payload = "hello"
+        emitter.on(triggerEvent,(p)=>{
+          expect(p).to.deep.equal(payload)
+          return done()
+        })
+        for(i=0;i<n;i++) {
+          emitter.emit(onEvent,payload)
+        }
+      })
+    })
+
+    describe('Every "x" instances of the event',()=>{
+      let k
+      let n = 10
+      let cycles = 2
+      before(()=>{
+        k = ksana.on(emitter,onEvent)
+        .afterNTimes(n)
+        .trigger(triggerEvent)
+      })
+      after(()=>{
+        k.stop()
+      })
+      it('Should trigger result event',(done)=>{
+        let payload = "hello"
+        let called = 0
+        emitter.on(triggerEvent,(p)=>{
+          expect(p).to.deep.equal(payload)
+          called = called + 1
+          if(called == cycles) {
+            return done()
+          }
+        })
+        for(i=0;i<n*cycles;i++) {
+          emitter.emit(onEvent,payload)
+        }
+      })
+    })
+
+    describe('After "x" instances and a few more of the event',()=>{
+      let k
+      let n = 10
+      let few = n/2
+      before(()=>{
+        k = ksana.on(emitter,onEvent)
+        .afterNTimes(n)
+        .trigger(triggerEvent)
+      })
+      after(()=>{
+        k.stop()
+      })
+      it('Should still trigger result event only once',(done)=>{
+        let payload = "hello"
+        let called = 0
+        emitter.on(triggerEvent,(p)=>{
+          expect(p).to.deep.equal(payload)
+          called = called + 1
+        })
+        for(i=0;i<n+few;i++) {
+          emitter.emit(onEvent,payload)
+        }
+        expect(called).to.be.equal(1)
+        return done()
+      })
+    })
+  })
+
+  describe('Within a time interval',()=>{
+    describe('"x" number of events are generated',()=>{
+      let k
+      before(()=>{
+        k = ksana.on(emitter,onEvent)
+        .slidingWindow(5)
+        .count()
+        .exceeds(10)
+        .trigger(triggerEvent)
+      })
+      after(()=>{
+        k.stop()
+      })
+      it('Should then generate trigger event',()=>{
+
       })
     })
   })
